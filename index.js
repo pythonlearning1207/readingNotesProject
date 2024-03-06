ff0b6cfa350a6e2e6ae4a357a7848c1473309fa3
Ray <xcz1207@gmail.com>
2024-03-05 22:26:01 -0800

    edit.ejs done.

--- index.js
+++ index.js
@@ -28,8 +28,14 @@ async function bookImg(isbn) {
 
 app.get("/", async(req, res) => {
     const result = await db.query("SELECT * FROM book INNER JOIN author ON book.author_id = author.id INNER JOIN note ON book.note_id = note.id;");
-    const data = result.rows;
-    res.render("index.ejs", { books: data });
+    const books = result.rows;
+    books.forEach(book =>{
+        if (book.read_date) {
+            const [year, month, day] = book.read_date.toISOString().split('T')[0].split('-');
+            book.read_date = `${month}/${day}/${year}`;
+        }
+    })
+    res.render("index.ejs", { books: books });
 });
 
 app.post("/new", async(req, res)=> {
@@ -40,9 +46,9 @@ app.post("/new", async(req, res)=> {
 //add new post 
 app.post("/add", async(req, res)=> {
     // retrive data from edit.ejs
-    const first_name = req.body.first_name;
-    const last_name = req.body.last_name;
-    const title = req.body.title;
+    const first_name = req.body.first_name ? req.body.first_name.trim() : '';
+    const last_name = req.body.last_name ? req.body.last_name.trim() : '';
+    const title = req.body.title ? req.body.title.trim() : '';
     const isbn = req.body.isbn;
     const brief = req.body.brief;
     const content = req.body.content;
@@ -60,7 +66,22 @@ app.post("/add", async(req, res)=> {
     console.log(my_rating);
     console.log(img_url);
     //inputting data to db
-
+    try {
+        // insert into author table returning id
+        const authorResult = await db.query("INSERT INTO author(first_name, last_name) VALUES($1,$2) RETURNING id;",[first_name, last_name]);
+        // obtain author_id
+        const author_id = authorResult.rows[0].id;
+        // insert into note table returning id
+        const noteResult = await db.query("INSERT INTO note(content, read_date, my_rating, brief) VALUES($1,$2,$3,$4) RETURNING id;",[content,read_date,my_rating,brief]);
+        // obtain note_id
+        const note_id = noteResult.rows[0].id;
+        // insert into book table
+        db.query("INSERT INTO book(title, isbn, purchasing_link, note_id, author_id, img_url) VALUES($1,$2,$3,$4,$5,$6);",[title,isbn,'amazon.com',note_id,author_id,img_url]);
+        console.log("Info added succesfully!")
+        res.redirect("/");
+    } catch (error) {
+        console.log(error);
+    }
 })
 // listen
 app.listen(port, (req, res)=> {
